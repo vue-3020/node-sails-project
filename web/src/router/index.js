48eb92efeed62b1a4be842b126d9347e5b8d1089
xinkai.wu@tescomm.com.cn <xinkai.wu@tescomm.com.cn>
2019-04-23 18:10:32 +0800

    2

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/login'
-
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/Login'
+    },
+    {
+      path: '/Login',
       name: 'Login',
-      component: Login
+      component: resolve => require(['#/login.vue'], resolve),
+      meta: {
+        title: '登录页面',
+        isUseCache: false,
+        keepAlive: false
+      }
+    },
+    {
+      path: '/404',
+      component: resolve => require(['&/common/404.vue'], resolve)
+    },
+    {
+      path: '*',
+      redirect: '/404'
     }
+    // {
+    //   path: '/',
+    //   component: resolve => require(['&/common/homePage.vue'], resolve),
+    //   meta: {
+    //     title: '首页',
+    //     isUseCache: false, // 结合activated钩子函数中的判断来确定是否去刷新数据
+    //   },
+    // },
   ]
 })
